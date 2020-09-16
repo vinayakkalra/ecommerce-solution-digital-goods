42f8a7b0b5f276a6290a1c67253782ae9c587ccf
amcba7 <amcba7@gmail.com>
2020-09-16 17:19:19 +0530

    login page update

--- template/header.php
+++ template/header.php
@@ -7,7 +7,7 @@
             </p>
         </div>
         <div id="top-bar-right" class="container-sm">
-            <a data-toggle="modal" data-target="#exampleModalCenter" href="#exampleModalCenter" class="font-Lato-Regular">Login</a>
+            <a href="login" class="font-Lato-Regular">Login</a>
             <a href="about.php" role="button" class="font-Lato-Regular">About us</a>
             <a href="contact" role="button" class="font-Lato-Regular">Contace us</a>
             <a id="shop" href="#" role="button" class="font-Lato-Regular">Shop</a>
@@ -41,70 +41,4 @@
 
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-    <div class="modal-content">
-      <!-- <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div> -->
 
-        
-
-      <div class="modal-body" style="padding:50px;">
-        <div class="row">
-            <div class="col-6 font-Lato-Regular" style="font-weight:bold;">
-                <h3 style="font-size:20px">LOGIN</h3>
-                <div style="font-size:14.4px;">
-                    <form>
-                        <div class="form-group">
-                            <label for="login-mail">Username or email address*</label>
-                            <input type="email" class="form-control" id="login-mail" aria-describedby="emailHelp">
-    
-                        </div>
-                        <div class="form-group">
-                            <label for="login-password">Password</label>
-                            <input type="password" class="form-control" id="login-password" placeholder="Password">
-                        </div>
-                        <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">Remember me</label>
-                        </div>
-                        <button type="button" class="btn" style="background-color:#24436e; color:white;">LOG IN</button>
-                        <p style="margin-top:10px;"><a href="">Lost your password</a></p>
-                    </form>            
-                </div>  
-            </div>
-            <div class="col-6 font-Lato-Regular"  style="font-weight:bold;">
-                <h3 style="font-size:20px">REGISTER</h3>
-                <div style="font-size:14.4px;">
-                    <form>
-                        <div class="form-group">
-                            <label for="register-mail">Email address*</label>
-                            <input type="email" class="form-control" id="register-mail" aria-describedby="emailHelp">
-    
-                        </div>
-                        <p>A password will be sent to your email address.</p>
-                        <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" id="registerCheck">
-                            <label class="form-check-label" for="registerCheck">Subscribe to our newsletter</label>
-                        </div>
-                        <p>
-                        Your personal data will be used to support your experience throughout this website, to manage access to your account, and for other purposes described in our 
-                        <a href="policy">privacy policy.</a>
-                        </p>
-                        <button type="button" class="btn" style="background-color:#24436e; color:white;">REGISTER</button>
-                    </form>            
-                </div>
-            </div>
-        </div>    
-      </div>
-      <!-- <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div> -->
-    </div>
-  </div>
-</div>
