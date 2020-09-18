f9068a2b6f078170d61592bfb7fbba5e9a6a9fe0
amcba7 <amcba7@gmail.com>
2020-09-18 18:06:50 +0530

    table

--- sql/user.sql
+++ sql/user.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 17, 2020 at 06:03 PM
+-- Generation Time: Sep 18, 2020 at 02:34 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.2.32
 
@@ -29,11 +29,24 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
   `email` varchar(200) NOT NULL,
   `number` int(200) NOT NULL,
   `password` int(200) NOT NULL,
-  `confirm password` int(200) NOT NULL
+  `confirm password` int(200) NOT NULL,
+  `from_ip` varchar(200) NOT NULL,
+  `from_browser` varchar(200) NOT NULL,
+  `time` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `email`, `number`, `password`, `confirm password`, `from_ip`, `from_browser`, `time`) VALUES
+(1, 'user', 'user@user.com', 123, 123, 123, '', '', ''),
+(2, 'user', 'user1@user.com', 123, 123, 123, '', '', ''),
+(3, 'user2', 'user2@user.com', 123, 123, 123, '', '', '');
 
 --
 -- Indexes for dumped tables
@@ -53,7 +66,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
