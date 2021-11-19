7798dc7b25ea412cf187ac54ece0bd98e4b00966
vinayakkalra <vinayak.kalra@quadbtech.com>
2021-11-20 02:18:47 +0530

    changes in config

--- php/config.php
+++ php/config.php
@@ -1,21 +1,14 @@
 <?php
+
+  // enter your connection string for database here
   $conn= new mysqli("localhost","root","","test_data_export");
-    // bulkdatabaseprovider
-  // $conn= new mysqli("localhost","bulkdatabaseprov","jd9vjmt0jE1r","bulkdatabaseprovider");
-  // $conn= new mysqli("localhost","parvinder","pzxuTAmsgGR3","test_content_engine");
-  
+   
   if($conn->connect_error){
       die("connection Failed" .$conn->connect_error);
   }
-
-
-  // razorpay Live Key
-  // $rzp_key = "rzp_live_gdsQLzKqW7UKZN";
-
-
-
+  
   // razorpay test key
-  $rzp_key = "rzp_test_mAMehuKH8LNrn8";
+  $rzp_key = "ENTER_YOUR_RAZORPAY_KEY_HERE";
 
   
 ?>
